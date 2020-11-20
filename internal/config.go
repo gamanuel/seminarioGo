85f06e841afc605cae81a407c931c0f3ffe6f79b
Manuel Garcia Amaro <manuelgarcia0952@gmail.com>
2020-11-20 11:56:44 -0300

    Inicio proyecto

---
+++ internal/config.go
@@ -0,0 +1,17 @@
+package config
+
+type DbConfig struct {
+	Driver string `yaml:"driver"`
+}
+
+type Config struct {
+	DB DbConfig `yaml:"db"`
+	Version string `yaml:"version"`
+}
+
+func LoadConfig(filename string) (*Config, error) {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+
+	}
+}
